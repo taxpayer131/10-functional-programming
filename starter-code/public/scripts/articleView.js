a9602e4c9cdd0f7b1f9343e149b2045ca02d107d
taxpayer131 <chris.stefan1844@gmail.com>
2017-07-24 10:09:31 -0700

    regular save

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -137,6 +137,7 @@ articleView.initAdminPage = function() {
   // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
   // we are then calling "template" on line 117.
+  Handlebars.compile($('#admin-handlebars').text());
 
   // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
   // appending to the DOM.
